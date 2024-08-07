8082aed8091ed4b6efa1414dae0823e621e937cb
patrick brisbin <pbrisbin@gmail.com>
2024-08-07 17:16:59 -0400

    Try it THIS way

--- netlify/edge-functions/hello.tsx
+++ netlify/edge-functions/hello.tsx
@@ -2,8 +2,11 @@ import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (_request: Request, _context: Context) => {
   const response = await fetch("https://github.com/restyled-io/restylers/releases/download/stable/restylers.yaml")
-  response.headers["Access-Allow-Origin"] = "*"
-  return response
+  return new Response(response.body, {
+    headers: {
+       'access-control-allow-origin': '*'
+    }
+  })
 }
 
 export const config: Config = { path: "/test" };
