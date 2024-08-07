d0bc094fb2aef79edf56d32c1f3a55297078b4d1
patrick brisbin <pbrisbin@gmail.com>
2024-08-07 16:06:24 -0400

    Experiment with edge-functions

---
+++ netlify/edge-functions/manifests.js
@@ -0,0 +1,16 @@
+const example =
+  "https://objects.githubusercontent.com/github-production-release-asset-2e65be/107553617/7e7edbf0-0130-4084-ab74-9e2ae5180c79?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20240807%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240807T195604Z&X-Amz-Expires=300&X-Amz-Signature=67a2f43175c9eaa1412b4eb347593e1dc247b9c58af7c318568d975dcfea2c05&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=107553617&response-content-disposition=attachment%3B%20filename%3Drestylers.yaml&response-content-type=application%2Foctet-stream";
+
+export default async (_req, _context) => {
+  new Response({
+    status: 301,
+    headers: {
+      Location: example,
+      "Access-Allow-Origin": "*",
+    },
+  });
+};
+
+export const config = {
+  path: "/data-files/restylers/manifests/*",
+};
