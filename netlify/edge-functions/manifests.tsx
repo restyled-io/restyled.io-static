91a4e37ac374c17ea2831c0df394094ae1963c43
patrick brisbin <pbrisbin@gmail.com>
2024-08-07 17:30:42 -0400

    Fix inversion that didn't matter

--- netlify/edge-functions/manifests.tsx
+++ netlify/edge-functions/manifests.tsx
@@ -4,7 +4,7 @@ declare var URLPattern: any;
 
 export default async (request: Request, _context: Context) => {
   const {pathname} = URLPattern.exec(request.url)
-  const suffix = pathname.slice("/data-files/manifests/restylers/".length)
+  const suffix = pathname.slice("/data-files/restylers/manifests/".length)
   const ghURL = `https://github.com/restyled-io/restylers/releases/download/${suffix}`
   const response = await fetch(ghURL)
   return new Response(response.body, {
