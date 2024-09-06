c88856e636cd8c9a8f87d989fd6d02a7a933210f
patrick brisbin <pbrisbin@gmail.com>
2024-09-06 10:19:46 -0400

    Add restyler-tags function

---
+++ netlify/edge-functions/restyler-tags.test.ts
@@ -0,0 +1,46 @@
+import handler from "./restyler-tags.js";
+
+describe("restyler-tags", () => {
+  it(
+    "Returns all non-SHA tags from DockerHub",
+    async () => {
+      const request = new Request(
+        "https://example.com/restyler-tags?name=fourmolu"
+      );
+
+      const response = await handler(request, {});
+      expect(response.ok).toBe(true);
+
+      // Assert on oldest 5 so, as we ship new tags, this test still passes
+      const json = await response.json();
+      expect(json.slice(-5)).toEqual([
+        "v0.14.1.0",
+        "v0.13.0.0",
+        "v0.12.0.0",
+        "v0.10.1.0",
+        "v0.4.0.0",
+      ]);
+    },
+    60 * 1000
+  );
+
+  it("Returns 400 for missing name param", async () => {
+    const request = new Request("https://example.com/restyler-tags");
+
+    const response = await handler(request, {});
+
+    expect(response.ok).toBe(false);
+    expect(response.status).toBe(400);
+  });
+
+  it("Returns 404 for non-existent Restyler names", async () => {
+    const request = new Request(
+      "https://example.com/restyler-tags?name=floobey"
+    );
+
+    const response = await handler(request, {});
+
+    expect(response.ok).toBe(false);
+    expect(response.status).toBe(404);
+  });
+});
---
+++ netlify/edge-functions/restyler-tags.ts
@@ -0,0 +1,97 @@
+declare var URL: any;
+
+import type { Context } from "@netlify/edge-functions";
+
+type Tag = {
+  name: string;
+};
+
+type Page = {
+  next: string | null | undefined;
+  results: Tag[];
+};
+
+class ResponseError extends Error {
+  status: number;
+  statusText: string;
+
+  constructor(status: number, statusText: string) {
+    super(`${status}: ${statusText}`);
+    this.name = "ResponseError";
+    this.status = status;
+    this.statusText = statusText;
+  }
+
+  toResponse(): Response {
+    return new Response(this.statusText, {
+      status: this.status,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  }
+}
+
+async function fetchPage(url: string): Promise<Page> {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new ResponseError(response.status, response.statusText);
+  }
+
+  const page: Page = await response.json();
+
+  return page;
+}
+
+async function fetchTags(
+  namespace: string,
+  repository: string
+): Promise<Tag[]> {
+  let page: Page = {
+    next: `https://hub.docker.com/v2/namespaces/${namespace}/repositories/${repository}/tags?page_size=100`,
+    results: [],
+  };
+
+  let results: Tag[] = [];
+
+  while (page.next !== null && page.next !== undefined) {
+    results = results.concat(
+      page.results.filter((tag) => !isCommitSha(tag.name))
+    );
+    page = await fetchPage(page.next);
+  }
+
+  return results;
+}
+
+function isCommitSha(name: string): boolean {
+  return name.match(/^[a-f0-9]{40}$/) !== null;
+}
+
+export default async (request: Request, _context: Context) => {
+  try {
+    const url = new URL(request.url);
+    const params: URLSearchParams = url.searchParams;
+    const name = params.get("name");
+
+    if (!name) {
+      throw new ResponseError(400, "name parameter is required");
+    }
+
+    const tags = await fetchTags("restyled", `restyler-${name}`);
+    const body = JSON.stringify(tags.map((tag) => tag.name));
+
+    return new Response(body, {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  } catch (e) {
+    if (e instanceof ResponseError) {
+      return e.toResponse();
+    }
+
+    throw e; // rethrow
+  }
+};
